88a9c3bc6853336e208f818fa0b2764a62d806af
David Hood <david@thoughtful.net.nz>
2018-10-09 17:04:00 +1300

    put data together

---
+++ all_the_tweets.R
@@ -0,0 +1,19 @@
+library(readr)
+library(dplyr)
+
+set1 = "../fprog/canadian_racists"
+set2 = "../fprog/mol_racist"
+set3 = "../fprog/South_racist"
+set4 = "../fprog/recent_direct"
+set5 = "../fprog/recent_direct_regather600"
+f1 <- paste0(set1, "/", list.files(set1, pattern=".csv$"))
+f2 <- paste0(set2, "/", list.files(set2, pattern=".csv$"))
+f3 <- paste0(set3, "/", list.files(set3, pattern=".csv$"))
+f4 <- paste0(set4, "/", list.files(set4, pattern=".csv$"))
+f5 <- paste0(set5, "/", list.files(set5, pattern=".csv$"))
+full_list = c(f1,f2,f3,f4,f5)
+
+v_for_visitor <- bind_rows(lapply(full_list, read_csv, col_types=cols(
+    .default = col_character()))) %>% distinct()
+
+
