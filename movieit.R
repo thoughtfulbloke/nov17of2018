e065bfd04c978756c2d698948716ca86e9e207cc
David Hood <david@thoughtful.net.nz>
2018-11-11 09:04:43 +1300

    better movie

--- movieit.R
+++ movieit.R
@@ -39,48 +39,62 @@ m_for_mentions <- v_for_visitor %>% select(created_at, user_id, mentions_user_id
     mutate(mentions = map(mentions_user_id, function(x){unlist(strsplit(x, split=" "))})) %>%
     unnest(mentions) %>% 
     mutate(NZ_time = with_tz(ymd_hms(created_at), tz="Pacific/Auckland"),
-           step15 = floor_date(NZ_time,unit = "15 minutes"))
+           step1 = floor_date(NZ_time,unit = "hour")) %>%
+    filter(step1 >= ISOdatetime(2018,8,1,0,0,0, tz="Pacific/Auckland"),
+           step1 < ISOdatetime(2018,8,4,0,0,0, tz="Pacific/Auckland"))
 
-# 3065709 mentions
+# 598612 mentions
 
-anyNZ <- m_for_mentions %>% left_join(layout, by=c("user_id" = "name")) %>%
+toNZer <- m_for_mentions %>% left_join(layout, by=c("user_id" = "name")) %>%
     rename(x1=x, y1=y) %>% left_join(layout, by=c("mentions" = "name")) %>%
-    rename(x2=x, y2=y)  %>% filter(!is.na(x1) | !is.na(x2)) %>%
-    count(user_id, mentions, x1, y1,x2,y2,step15) %>% mutate(scaled_alpha = n/max(n))
+    rename(x2=x, y2=y)  %>% filter(!is.na(x2)) %>%
+    count(user_id, mentions, x1, y1,x2,y2,step1)
 
-# 185756
+# sum(toNZer$n) 44062
 
-allNZ <- anyNZ %>% filter(!is.na(x1) & !is.na(x2))
-# 31818 messages
+allNZ <- toNZer %>% filter(!is.na(x1))
+# sum(allNZ$n) 19111 messages
+
 messagingNZ <- layout %>% filter(name %in% c(allNZ$user_id, allNZ$mentions))
 
-# 1505 NZers
+# 1117 NZers
 anibase <- ggplot(data=messagingNZ, aes(x=x,y=y)) + geom_point(size=0.05) + theme_void() +
-    geom_segment(data=allNZ, aes(x=x1, y=y1, xend=x2, yend=y2, alpha=scaled_alpha)) + 
+    geom_segment(data=allNZ, aes(x=x1, y=y1, xend=x2, yend=y2), lwd=0.1) + 
     theme(legend.position = "none") +
-    labs(title = 'total 1505 NZ accounts & 67836 notifications, NZTime: {frame_time}') +
-    transition_time(step15) +
+    labs(title = 'total 1117 NZ accounts & 19111 notifications, NZTime: {frame_time}') +
+    transition_time(step1) +
     enter_fade() + 
     exit_fade()
 
-animate(anibase, nframes = length(unique(allNZ$step15)))
+animate(anibase, nframes = length(unique(allNZ$step1)))
 anim_save("NZ.gif")
     
-toNZ <- anyNZ  %>% filter(is.na(x1) & !is.na(x2)) %>%
-    group_by(mentions,x2,y2,step15) %>% 
+from_oe <- toNZer  %>% filter(is.na(x1)) %>%
+    group_by(mentions,x2,y2,step1) %>% 
     summarise(n=sum(n)) %>% ungroup() %>%
-    mutate(scaled_size=log10(n) + .001)
-# 67715
+    rename(n_oe = n)
+from_nz <- toNZer  %>% filter(!is.na(x1)) %>%
+    group_by(mentions,x2,y2,step1) %>% 
+    summarise(n=sum(n)) %>% ungroup() %>%
+    rename(n_nz = n)
+
+from_all <- from_oe %>%
+    full_join(from_nz) %>%
+    mutate(n_oe = ifelse(is.na(n_oe), 0, n_oe),
+           n_nz = ifelse(is.na(n_nz), 0, n_nz),
+           n_total = n_oe + n_nz,
+           log10mentions = log10(n_total),
+           oe_prop = n_oe / n_total)
 
 plusbase <- ggplot(data=messagingNZ, aes(x=x,y=y)) + geom_point(size=0.05) + theme_void() +
-        geom_segment(data=allNZ, aes(x=x1, y=y1, xend=x2, yend=y2, alpha=scaled_alpha)) +
-    geom_point(data=toNZ, aes(x=x2,y=y2, size=scaled_size), colour="red") +
-        theme(legend.position = "none") +
-        labs(title = '1505 NZ accounts & 67836 NZ 67715 nonNZ notifications, {frame_time}NZ') +
-        transition_time(step15) +
+        geom_segment(data=allNZ, aes(x=x1, y=y1, xend=x2, yend=y2), lwd=0.2) +
+    geom_point(data=from_all, aes(x=x2,y=y2, size=log10mentions, colour=oe_prop)) +
+    scale_colour_viridis_c() +
+        labs(title = '1117 NZ accounts & 34105 notifications, {frame_time} NZtime') +
+        transition_time(step1) +
         enter_appear() + 
         exit_disappear()
     
-animate(plusbase, nframes = length(unique(allNZ$step15)))
+animate(plusbase, nframes = length(unique(allNZ$step1)) * 6)
 anim_save("NZplus.gif")
 
